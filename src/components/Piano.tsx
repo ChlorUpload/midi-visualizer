a939331676afde181c8f97a460b13634935556cf
chlorUpload <chlor.upload@gmail.com>
2021-03-31 02:46:14 +0900

    코드 정리

--- src/components/Piano.tsx
+++ src/components/Piano.tsx
@@ -1,54 +1,15 @@
 import { useEffect, useState } from "react";
 import produce from "immer";
 import { GroupProps } from "react-three-fiber";
-
-export enum ToneName {
-  C = 0,
-  CSharp,
-  D,
-  DSharp,
-  E,
-  F,
-  FSharp,
-  G,
-  GSharp,
-  A,
-  ASharp,
-  B,
-}
-
-export type Tone = {
-  tone: ToneName;
-  octave: number;
-};
-
-type CuboidInfo = {
-  x: number;
-  y: number;
-  z: number;
-  xLen: number;
-  yLen: number;
-  zLen: number;
-};
-
-function chromaticTodiatonic(name: ToneName): number {
-  const map: { name: ToneName; ind: number }[] = [
-    {
-      name: ToneName.CSharp,
-      ind: 0,
-    },
-    { name: ToneName.DSharp, ind: 1 },
-    { name: ToneName.E, ind: 2 },
-    { name: ToneName.FSharp, ind: 3 },
-    { name: ToneName.GSharp, ind: 4 },
-    { name: ToneName.ASharp, ind: 5 },
-    { name: ToneName.B, ind: 6 },
-  ];
-  for (const m of map) {
-    if (name <= m.name) return m.ind;
-  }
-  return -1;
-}
+import {
+  chromaticTodiatonic,
+  getTones,
+  isNaturalTone,
+  isValidTone,
+  Tone,
+  ToneName,
+} from "../utils/tone";
+import { CuboidInfo } from "../utils/cuboid";
 
 function getCuboidInfo(
   tone: Tone,
@@ -80,43 +41,6 @@ function getCuboidInfo(
   }
 }
 
-function isNaturalTone(tone: Tone): boolean {
-  switch (tone.tone) {
-    case ToneName.CSharp:
-    case ToneName.DSharp:
-    case ToneName.FSharp:
-    case ToneName.GSharp:
-    case ToneName.ASharp:
-      return false;
-    default:
-      return true;
-  }
-}
-
-function isValidTone(tone: Tone | undefined): boolean {
-  if (tone === undefined) return false;
-
-  const ind = tone.tone + tone.octave * 12;
-
-  // 최소인 A0의 경우 9, 최대인 C8의 경우 96
-  return ind >= 9 && ind <= 96;
-}
-
-function getTones(startTone: Tone, endTone: Tone): Tone[] {
-  const startInd = startTone.tone + startTone.octave * 12;
-  const endInd = endTone.tone + endTone.octave * 12;
-
-  const retTones: Tone[] = [];
-  for (let i = startInd; i <= endInd; i++) {
-    retTones.push({
-      octave: Math.floor(i / 12),
-      tone: i % 12,
-    });
-  }
-
-  return retTones;
-}
-
 export default function Piano({
   width,
   startNatural: start,
@@ -141,7 +65,6 @@ export default function Piano({
         : {
             tone: ToneName.A,
             octave: 0,
-            sharp: false,
           };
     const endTone =
       isValidTone(end) && isNaturalTone(end as Tone)
@@ -149,7 +72,6 @@ export default function Piano({
         : {
             tone: ToneName.C,
             octave: 8,
-            sharp: false,
           };
 
     const _tones = getTones(startTone, endTone);
